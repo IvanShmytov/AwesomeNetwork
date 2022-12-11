747c1b2131827e055cf6dfd8b6ba7e4ee075c164
Ivan <ivanshmytov@mail.ru>
2022-12-11 16:31:37 +0400

    Выполнено задание 35.6.4.

--- AwesomeNetwork/Controllers/HomeController.cs
+++ AwesomeNetwork/Controllers/HomeController.cs
@@ -9,23 +9,33 @@ using AwesomeNetwork.Models;
 using AwesomeNetwork.Data.UoW;
 using AutoMapper;
 using AwesomeNetwork.ViewModels.Account;
+using Microsoft.AspNetCore.Identity;
+using AwesomeNetwork.Models.Users;
 
 namespace AwesomeNetwork.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
+        private readonly SignInManager<User> _signInManager;
+        public HomeController(ILogger<HomeController> logger, SignInManager<User> signInManager)
         {
             _logger = logger;
+            _signInManager = signInManager;
         }
 
         [Route("")]
         [Route("[controller]/[action]")]
         public IActionResult Index()
         {
-            return View(new MainViewModel());
+            if (_signInManager.IsSignedIn(User))
+            {
+                return RedirectToAction("MyPage", "AccountManager");
+            }
+            else
+            {
+                return View(new MainViewModel());
+            }
         }
 
         [Route("[action]")]
